df0ac52259ccbd8fb7dc4fb58ce5247ea4ddfdf4
David Zeit <dzeit216@gmail.com>
2018-04-26 16:26:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+counter = i 
+
+def countdown(number)
+  while counter > 0 
+    puts "#{number} SECOND(S)!"
+    counter -= 1 
+  end 
+  "HAPPY NEW YEAR!"
 end
